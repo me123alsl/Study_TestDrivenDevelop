c240b9b1feb55fde1032656c2e35bd46148177ab
JinseokSong <me123alsl@gmail.com>
2023-10-06 05:54:26 +0900

    상품 조회 기능에 대한 API 테스트 생성

--- src/test/java/com/example/productorderservice/product/ProductApiTest.java
+++ src/test/java/com/example/productorderservice/product/ProductApiTest.java
@@ -2,10 +2,15 @@ package com.example.productorderservice.product;
 
 
 import com.example.productorderservice.ApiTest;
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+
+import static org.assertj.core.api.Assertions.*;
 
 public class ProductApiTest extends ApiTest {
 
@@ -18,7 +23,23 @@ public class ProductApiTest extends ApiTest {
 
         final var response = ProductSteps.상품등록요청(request);
 
-        Assertions.assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+    }
+
+    @Test
+    void 상품조회(){
+        ProductSteps.상품등록요청(ProductSteps.상품등록_생성());
+        long productId = 1L;
+
+        ExtractableResponse<Response> response = RestAssured.given().log().all()
+                .when()
+                .get("/products/{productId}", productId)
+                .then()
+                .log().all()
+                .extract();
+
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+        assertThat(response.jsonPath().getString("name")).isEqualTo("상품명");
     }
 
 
