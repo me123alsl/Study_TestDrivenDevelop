a6b668e0371959b6104cb3233ad1f70339458bdb
JinseokSong <me123alsl@gmail.com>
2023-10-06 05:48:21 +0900

    상품 조회 기능에 대한 테스트 생성

--- src/test/java/com/example/productorderservice/product/ProductApiTest.java
+++ src/test/java/com/example/productorderservice/product/ProductApiTest.java
@@ -2,14 +2,10 @@ package com.example.productorderservice.product;
 
 
 import com.example.productorderservice.ApiTest;
-import io.restassured.RestAssured;
-import io.restassured.response.ExtractableResponse;
-import io.restassured.response.Response;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 
 public class ProductApiTest extends ApiTest {
 
@@ -18,29 +14,11 @@ public class ProductApiTest extends ApiTest {
 
     @Test
     void 상품등록() {
-        final var request = 상품등록_생성();
+        final var request = ProductSteps.상품등록_생성();
 
-        final var response = 상품등록요청(request);
+        final var response = ProductSteps.상품등록요청(request);
 
         Assertions.assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
-    }
-
-    private static ExtractableResponse<Response> 상품등록요청(final AddProductRequest request) {
-        return RestAssured.given().log().all()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .body(request)
-                .when()
-                .post("/products")
-                .then()
-                .log().all().extract();
-    }
-
-    private static AddProductRequest 상품등록_생성() {
-        final String name = "상품명";
-        final int price = 1000;
-        final DiscountPolicy discountPolicy = DiscountPolicy.NONE;
-
-        return new AddProductRequest(name, price, discountPolicy);
     }
 
 
