6b0e08e5551f1191940632bed924a40533591d02
JinseokSong <me123alsl@gmail.com>
2023-10-09 10:58:51 +0900

    주문 생성API 테스트 생성

--- src/main/java/com/example/productorderservice/order/OrderService.java
+++ src/main/java/com/example/productorderservice/order/OrderService.java
@@ -1,9 +1,14 @@
 package com.example.productorderservice.order;
 
 import com.example.productorderservice.product.Product;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Component
+@RestController
 class OrderService {
 
     private final OrderPort orderPort;
@@ -11,9 +16,12 @@ class OrderService {
     public OrderService(OrderPort orderPort){
         this.orderPort = orderPort;
     }
-    public void createOrder(final CreateOrderRequest request) {
+
+    @PostMapping("/orders")
+    public ResponseEntity<Void> createOrder(@RequestBody final CreateOrderRequest request) {
         final Product product = orderPort.getProductById(request.productId());
         final Order order = new Order(product, request.quantity());
         orderPort.save(order);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 }
