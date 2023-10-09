5a84e2583d7a2124165cb39906824d01c3626b19
JinseokSong <me123alsl@gmail.com>
2023-10-09 11:05:36 +0900

    주문 생성 JPA 테스트 생성

--- src/main/java/com/example/productorderservice/order/Order.java
+++ src/main/java/com/example/productorderservice/order/Order.java
@@ -1,33 +1,30 @@
 package com.example.productorderservice.order;
 
 import com.example.productorderservice.product.Product;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.util.Assert;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name= "orders")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
 class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private final Product product;
-    private final int quantity;
+
+    @OneToOne
+    private Product product;
+    private int quantity;
 
     public Order(final Product product, final int quantity) {
-        this.product = product;
-        this.quantity = quantity;
         Assert.notNull(product, "상품은 필수입니다.");
         Assert.isTrue(quantity > 0, "상품 수량은 0보다 커야 합니다.");
-    }
-
-    public void assignId(final Long id) {
-        this.id = id;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public Product getProduct() {
-        return product;
-    }
-
-    public int getQuantity() {
-        return quantity;
+        this.product = product;
+        this.quantity = quantity;
     }
 }
--- src/test/java/com/example/productorderservice/order/OrderApiTest.java
+++ src/test/java/com/example/productorderservice/order/OrderApiTest.java
@@ -1,44 +1,22 @@
 package com.example.productorderservice.order;
 
 import com.example.productorderservice.ApiTest;
-import com.example.productorderservice.product.DiscountPolicy;
-import com.example.productorderservice.product.Product;
-import com.example.productorderservice.product.ProductService;
 import com.example.productorderservice.product.ProductSteps;
-import io.restassured.RestAssured;
-import io.restassured.response.ExtractableResponse;
-import io.restassured.response.Response;
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
+import org.springframework.http.HttpStatus;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class OrderApiTest extends ApiTest {
 
     @Test
     void 상품주문() {
         ProductSteps.상품등록요청(ProductSteps.상품등록요청_생성());
-        final CreateOrderRequest request = 상품주문요청_생성();
+        final var request = OrderSteps.상품주문요청_생성();
 
-        ExtractableResponse<Response> response = RestAssured.given().log().all()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .body(request)
-                .when()
-                .post("/orders")
-                .then()
-                .log().all().extract();
+        final var response = OrderSteps.상품주문요청_생성(request);
 
-        assertThat(response.statusCode()).isEqualTo(201);
-    }
-
-    private static CreateOrderRequest 상품주문요청_생성() {
-        final long productId = 1L;
-        final int quantity = 2;
-        return new CreateOrderRequest(productId, quantity);
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
     }
 
 }
